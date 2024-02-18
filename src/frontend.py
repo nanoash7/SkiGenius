57119cda783c4f6a6856755803035d736d3fd3c9
nanoash7 <ashwin@nareshfamily.org>
2024-02-17 23:39:22 -0800

    Update frontend.py

--- src/frontend.py
+++ src/frontend.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: black;'>Ski Resort Recommendation Tool</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>US Ski Resort Recommendation Tool</h1>", unsafe_allow_html=True)
 
 state = st.text_input('State', '')
 price_range = st.slider('Select a Price Range (USD)', 0.0, 300.0, (100.0, 150.0))
@@ -9,5 +9,6 @@ if st.button('Get Results'):
     # Query goes here
     st.session_state['state'] = state
     st.session_state['price_range'] = price_range
-    
-st.text_area(label="Results", value="Show Results Here", height=200)
+
+result = st.session_state['result'] if 'result' in st.session_state else ""
+st.text_area(label="Results", value=result, height=200)
