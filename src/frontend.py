4ca17167455c889d7eef3fe89a1d8f61acd6b052
nanoash7 <ashwin@nareshfamily.org>
2024-02-19 19:03:30 -0800

    Update frontend.py
    front end input for location, price, skill level, and date

--- src/frontend.py
+++ src/frontend.py
@@ -1,14 +1,20 @@
 import streamlit as st
+import datetime as dt
 
-st.markdown("<h1 style='text-align: center; color: black;'>US Ski Resort Recommendation Tool</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Ski Genius: A US Ski Resort Recommendation Tool</h1>", unsafe_allow_html=True)
 
-state = st.text_input('State', '')
-price_range = st.slider('Select a Price Range (USD)', 0.0, 300.0, (100.0, 150.0))
+location_option = st.selectbox('Select a Location Option', ('Current Location (City)', 'Resort Location (State)'))
+location = st.text_input(location_option, '')
+max_price = st.slider('Select a Max Price (USD)', 0, 300, 100)
+skill_level = st.slider('Skill Level', 0, 100, 50)
+target_date = st.date_input("Target Date", value=None)
 
 if st.button('Get Results'):
+    st.session_state['location_option'] = location_option
+    st.session_state['location'] = location
+    st.session_state['max_price'] = max_price
+    st.session_state['skill_level'] = skill_level
+    st.session_state['target_date'] = target_date
     # Query goes here
-    st.session_state['state'] = state
-    st.session_state['price_range'] = price_range
 
 result = st.session_state['result'] if 'result' in st.session_state else ""
-st.text_area(label="Results", value=result, height=200)
