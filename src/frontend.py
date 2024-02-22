11a57733cb0ce3383ba04b7113965c641f9c3c98
nanoash7 <ashwin@nareshfamily.org>
2024-02-22 02:25:30 -0800

    front end querying and display results

--- src/frontend.py
+++ src/frontend.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 from frontendutil import build_query_vector
+from query_unit import query
+
+NUM_RESULTS = 15
 
 st.markdown("<h1 style='text-align: center; color: black;'>Ski Genius: A US Ski Resort Recommendation Tool</h1>", unsafe_allow_html=True)
 
@@ -8,6 +12,13 @@ location = st.text_input(location_option, '')
 max_price = st.slider('Select a Max Price (USD)', 0, 300, 100)
 skill_level = st.slider('Skill Level', 0, 100, 50)
 target_date = st.date_input("Target Date", value=None)
+
+def create_display(result):
+    df = pd.DataFrame(result)
+    df = df.drop(columns= ['_id', 'Snow', 'score'])
+    df.rename(columns={'Name': 'Resort Name', 'Price': 'Average Price (USD)'}, inplace=True)
+    df = df.reset_index(drop=True)
+    st.session_state['result'] = df
 
 if st.button('Get Results'):
     st.session_state['location_option'] = location_option
@@ -20,5 +31,10 @@ if st.button('Get Results'):
     if st.session_state['query'] == -1:
         st.error("Invalid Location")
     else:
-        # Perform the search using st.session_state['query'] and st.session_state['max_price']
-        pass
+        # Perform the query
+        result = query(st.session_state['query'], st.session_state['max_price'], NUM_RESULTS)
+        create_display(result)
+
+if 'result' in st.session_state:
+    st.markdown("<h3 style='text-align: center; color: black;'>Curated results based on your location, experience level, price constraints, and projected snow quality</h1>", unsafe_allow_html=True)
+    st.table(st.session_state['result'])
--- src/query_unit.py
+++ src/query_unit.py
@@ -17,7 +17,7 @@ client = connect_to_db(URI)
 db = client["ski_info"]
 collection = db["resorts"]
 
-def query(user_pref_vector):
+def query(user_pref_vector, max_price, num_results):
     """
     Function to query db with user inputs from front-end
 
@@ -25,9 +25,22 @@ def query(user_pref_vector):
     list user_pref_vector: vector containing user preferences
 
     Outputs:
-    list results : list of dictionaries containing top 10 neighbors
+    list results : list of dictionaries containing top num_results neighbors
 
     """
-    pipeline = create_pipeline(user_pref_vector,10)
+    pipeline = create_pipeline(user_pref_vector, num_results)
     results = query_neighbors(collection,pipeline)
-    return results
+
+    n_results = list(results)
+    price_filtered_results = list()
+
+    for obj in n_results:
+        if float(obj["Price"]) <= max_price:
+            price_filtered_results.append(obj)
+
+    def snow_compare(obj):
+        return float(obj["Snow"])
+
+    snow_sorted_results = sorted(price_filtered_results, key=snow_compare, reverse=True)
+
+    return snow_sorted_results
